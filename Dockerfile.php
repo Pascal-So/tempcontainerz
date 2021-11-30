a2dea750da76c5cf5cf5172a3172bafbcc42dbd8
Pascal Sommer <p@pascalsommer.ch>
2021-12-01 00:06:41 +0100

    add gd extension to php container

--- Dockerfile.php
+++ Dockerfile.php
@@ -5,6 +5,14 @@ FROM php:apache
 # Mount your applicaton root to /var/www/html
 # to serve it on port 80.
 
+RUN apt-get update && apt-get install -y \
+        libfreetype-dev \
+        libjpeg-dev \
+        libpng-dev \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
+    && docker-php-ext-install -j$(nproc) gd \
+    && rm -rf /var/lib/apt/lists/*
+
 SHELL ["/bin/bash", "-c"]
 RUN ln -s ../mods-available/{expires,headers,rewrite}.load /etc/apache2/mods-enabled/
 RUN sed -e '/<Directory \/var\/www\/>/,/<\/Directory>/s/AllowOverride None/AllowOverride All/' -i /etc/apache2/apache2.conf
